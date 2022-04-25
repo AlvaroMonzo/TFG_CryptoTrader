c04713a064e24a9d8092316f9f81a5430cf6e3b1
alvaro <alv123456@outlook.com>
2022-04-25 17:44:44 +0200

    Git ya creado de Binance para ver sus conexiones

--- TFG/src/tfg/Transaccion.java
+++ TFG/src/tfg/Transaccion.java
@@ -9,13 +9,8 @@ package tfg;
  *
  * @author alv12
  */
-public class TFG {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+public class Transaccion extends Thread{
+    double capitalInicial;
+    
     
 }
